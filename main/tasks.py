59ed36ae21eb3d99ecaedc197e5272cc57470986
Tengiz Pataraia <pataraiamistera@gmail.com>
2021-03-26 00:12:12 +0400

    d

--- main/tasks.py
+++ main/tasks.py
@@ -1,11 +1,18 @@
 from celery import shared_task
 from .models import EntryModel
 
+def send_mail(email,url,price):
+    return None
+
 @shared_task
 def check():
     entries = EntryModel.objects.all()
     for entry in entries:
-        print(entry.price)
-        print(entry.site.price_function)
-    print("checked")
+        wanted_price = entry.price
+        current_price = entry.site.price_function
+        
+        if current_price < wanted_price:
+            send_mail(entry.email,entry.url,untry.price)
+            #entry.delete()
+
     return "checked"
