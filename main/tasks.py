f6d7d400fa69b2c5c3be01653e66174c46776676
Tengiz Pataraia <pataraiamistera@gmail.com>
2021-03-26 12:32:49 +0400

    d

--- main/tasks.py
+++ main/tasks.py
@@ -1,8 +1,31 @@
 from celery import shared_task
 from .models import EntryModel
+import smtplib
 
 def send_mail(email,url,price):
-    return None
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+    server.login('karlo.tevzadze.1@iliauni.edu.ge', 'hmhltdcchueknufd')
+
+    subject = 'Price fell down'
+    body = f'''
+    Url: {url}
+    Price : {price}
+    '''
+
+    msg = f'Subject: {subject}\n\n{body}'
+
+    server.sendmail(
+        'karlo.tevzadze.1@iliauni.edu.ge',
+        email,
+        msg
+    )
+    print("Email Sent")
+
+    server.quit()
 
 @shared_task
 def check():
@@ -13,6 +36,6 @@ def check():
         
         if current_price < wanted_price:
             send_mail(entry.email,entry.url,untry.price)
-            #entry.delete()
+            entry.delete()
 
     return "checked"
