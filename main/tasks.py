1c901fa7f95b7f8081db9b0e38c12f546c728c44
Tengiz Pataraia <pataraiamistera@gmail.com>
2021-03-26 13:36:09 +0400

    d

--- main/tasks.py
+++ main/tasks.py
@@ -1,6 +1,8 @@
 from celery import shared_task
 from .models import EntryModel
 import smtplib
+from bs4 import BeautifulSoup
+import requests
 
 def send_mail(email,url,price):
     server = smtplib.SMTP('smtp.gmail.com', 587)
