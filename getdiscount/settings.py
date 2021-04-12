30da07870e8ec9aade4fcfc71d6d6038db97c024
mistera <pataraiamistera@gmail.com>
2021-04-12 15:50:28 +0400

    d

--- getdiscount/settings.py
+++ getdiscount/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'wiq6n$rwm-=7v^dcgydrofbg6-&l_k+te0emk#6^ata$-tf8%b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [46.101.243.16]
+ALLOWED_HOSTS = ["46.101.243.16"]
 
 
 # Application definition
